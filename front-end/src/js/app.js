61671c5f7174a0c30ff87889ce60fb242d52b4ae
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-08 21:55:34 +0530

    Implement download logics

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -71,8 +71,6 @@ function loadAllImages() {
         imageUrlList.forEach(imageUrl => {
             const divElm = $(`<div class="image"></div>`);
             divElm.css('background-image', `url(${imageUrl})`);
-            // console.log(imageUrl);
-            const imageName = imageUrl.replace(`${REST_API_URL}/images/`, "");
             const btn = $(`<button class="btn btn-light downloadBtn m-1 d-none ">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
                                      <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"/>
@@ -81,9 +79,6 @@ function loadAllImages() {
                            </button>`);
 
             divElm.append(btn);
-            divElm.on('click',(evt)=> {
-                $(evt.target).find('button').removeClass('d-none');
-            })
 
             divElm.on('mouseenter', (evt) => {
                 $(evt.target).find('button').removeClass('d-none');
@@ -93,6 +88,8 @@ function loadAllImages() {
                 $(evt.target).find('button').addClass('d-none');
             });
 
+            /*Download button click event*/
+
             mainElm.append(divElm);
         });
     });
@@ -100,3 +97,10 @@ function loadAllImages() {
     });
 }
 
+mainElm.on('click', 'button', (evt) => {
+    const downUrl = $(evt.target).parents('div').css('background-image').replace('url("', '').replace('")', '');;
+    const imageName = downUrl.replace(`${REST_API_URL}/images/`, "");
+    // console.log(downUrl);
+    // console.log(imageName);
+    saveAs(downUrl, imageName);
+});
