e34782bc2c796557c002aa86950e1c921a471c61
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-08 21:27:11 +0530

    Add download button

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -32,7 +32,8 @@ dropZoneElm.on('drop', (evt) => {
     uploadImages(imageFiles);
 });
 mainElm.on('click', '.image:not(.loader)', (evt)=> {
-    evt.target.requestFullscreen();
+    // console.log(evt.target);
+    // evt.target.requestFullscreen();
 });
 
 function uploadImages(imageFiles){
@@ -68,6 +69,27 @@ function loadAllImages() {
         imageUrlList.forEach(imageUrl => {
             const divElm = $(`<div class="image"></div>`);
             divElm.css('background-image', `url(${imageUrl})`);
+
+            const btn = $(`<button class="btn btn-light downloadBtn m-1 d-none ">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+                                     <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"/>
+                                </svg>
+                                
+                           </button>`);
+
+            divElm.append(btn);
+            divElm.on('click',(evt)=> {
+                $(evt.target).find('button').removeClass('d-none');
+            })
+
+            divElm.on('mouseenter', (evt) => {
+                $(evt.target).find('button').removeClass('d-none');
+            });
+
+            divElm.on('mouseleave', (evt) => {
+                $(evt.target).find('button').addClass('d-none');
+            });
+
             mainElm.append(divElm);
         });
     });
